dfa1ffa86055a55c0aa85403954dac6ba315081d
Sebastian Josef Kerres <sebastian.kerres@rwth-aachen.de>
2020-04-20 17:22:11 +0200

    feat: v7_1 [480x360] benchmark for transforms

--- project/SSD/ssd/config/defaults.py
+++ project/SSD/ssd/config/defaults.py
@@ -29,24 +29,24 @@ cfg.MODEL.PRIORS = CN()
 # cfg.MODEL.PRIORS.STRIDES = [[8,8], [16, 16], [30, 30], [60, 60], [100, 100], [300, 300]] #[300x300] resnet instructor v2
 # cfg.MODEL.PRIORS.FEATURE_MAPS = [[40, 30], [20, 15], [10, 8], [5, 5], [3, 3], [1, 1]] #[320x240] resnet instructor, resnet152, googlenet
 # cfg.MODEL.PRIORS.STRIDES = [[8, 8], [16, 16], [32, 30], [64, 48], [107, 80], [320, 240]]  #[320x240] resnet instructor, resnet152, googlenet
-# cfg.MODEL.PRIORS.FEATURE_MAPS = [[60, 45], [30, 23], [15, 12], [7, 7], [4, 4], [1, 1]] #[480x320] resnet instructor, resnet152, googlenet
-# cfg.MODEL.PRIORS.STRIDES = [[8, 8], [16, 15.65], [32, 30], [68.57, 51.43], [120, 90], [480, 360]]  #[480x320] resnet instructor, resnet152, googlenet
-cfg.MODEL.PRIORS.FEATURE_MAPS = [[90, 68],  [45, 34],       [23, 17],       [11, 9],        [6, 5],     [3, 3],         [1, 1]] #[720x560] resnet instructor, resnet152, googlenet
-cfg.MODEL.PRIORS.STRIDES = [[8, 8.23],      [16, 16.47],    [31.3, 32.94],  [65.45, 62.22], [120, 112], [240, 186.66],  [720,560]] #[720x560] resnet instructor, resnet152, googlenet
+cfg.MODEL.PRIORS.FEATURE_MAPS = [[60, 45], [30, 23], [15, 12], [7, 7], [4, 4], [1, 1]] #[480x320] resnet instructor, resnet152, googlenet
+cfg.MODEL.PRIORS.STRIDES = [[8, 8], [16, 15.65], [32, 30], [68.57, 51.43], [120, 90], [480, 360]]  #[480x320] resnet instructor, resnet152, googlenet
+# cfg.MODEL.PRIORS.FEATURE_MAPS = [[90, 68],  [45, 34],       [23, 17],       [11, 9],        [6, 5],     [3, 3],         [1, 1]] #[720x560] resnet instructor, resnet152, googlenet
+# cfg.MODEL.PRIORS.STRIDES = [[8, 8.23],      [16, 16.47],    [31.3, 32.94],  [65.45, 62.22], [120, 112], [240, 186.66],  [720,560]] #[720x560] resnet instructor, resnet152, googlenet
 
-# cfg.MODEL.PRIORS.MIN_SIZES = [30, 60, 111, 162, 213, 264] # [300x300], [320x240], [480x320]
-# cfg.MODEL.PRIORS.MAX_SIZES = [60, 111, 162, 213, 264, 315] # [300x300], [320x240], [480x320]
+cfg.MODEL.PRIORS.MIN_SIZES = [30, 60, 111, 162, 213, 264] # [300x300], [320x240], [480x320]
+cfg.MODEL.PRIORS.MAX_SIZES = [60, 111, 162, 213, 264, 315] # [300x300], [320x240], [480x320]
 # cfg.MODEL.PRIORS.MIN_SIZES = [30, 60, 111, 162, 213, 264, 315] # [720x560] resnet instructor small boxes
 # cfg.MODEL.PRIORS.MAX_SIZES = [60, 111, 162, 213, 264, 315, 500] # [720x560] resnet instructor small boxes
 
-cfg.MODEL.PRIORS.MIN_SIZES = [30, 67, 135, 250, 162, 365, 594] #[720x560] resnet instructor
-cfg.MODEL.PRIORS.MAX_SIZES = [67, 135, 250, 162, 365, 594, 709] #[720x560] resnet instructor
-# cfg.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2, 3], [2, 3], [2, 3], [2], [2]] #[300x300], [320x240], [480x320]
-cfg.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2, 3], [2, 3], [2, 3], [2, 3], [2], [2]] #[720x560]
+# cfg.MODEL.PRIORS.MIN_SIZES = [30, 67, 135, 250, 162, 365, 594] #[720x560] resnet instructor
+# cfg.MODEL.PRIORS.MAX_SIZES = [67, 135, 250, 162, 365, 594, 709] #[720x560] resnet instructor
+cfg.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2, 3], [2, 3], [2, 3], [2], [2]] #[300x300], [320x240], [480x320]
+# cfg.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2, 3], [2, 3], [2, 3], [2, 3], [2], [2]] #[720x560]
 # When has 1 aspect ratio, every location has 4 boxes, 2 ratio 6 boxes.
 # #boxes = 2 + #ratio * 2
-# cfg.MODEL.PRIORS.BOXES_PER_LOCATION = [4, 6, 6, 6, 4, 4]  # number of boxes per feature map location
-cfg.MODEL.PRIORS.BOXES_PER_LOCATION = [4, 6, 6, 6, 6, 4, 4]  # number of boxes per feature map location [720, 560] resnet instructor
+cfg.MODEL.PRIORS.BOXES_PER_LOCATION = [4, 6, 6, 6, 4, 4]  # number of boxes per feature map location [300x300], [320x240], [480x320]
+# cfg.MODEL.PRIORS.BOXES_PER_LOCATION = [4, 6, 6, 6, 6, 4, 4]  # number of boxes per feature map location [720, 560] resnet instructor
 cfg.MODEL.PRIORS.CLIP = True
 
 # -----------------------------------------------------------------------------
@@ -111,7 +111,7 @@ cfg.TEST.BATCH_SIZE = 10
 # ---------------------------------------------------------------------------- #
 # Specific test options
 # ---------------------------------------------------------------------------- #
-cfg.EVAL_STEP = 4000 # Evaluate dataset every eval_step, disabled when eval_step < 0
+cfg.EVAL_STEP = 1000 # Evaluate dataset every eval_step, disabled when eval_step < 0
 cfg.MODEL_SAVE_STEP = 15000 # Save checkpoint every save_step
 cfg.LOG_STEP = 10 # Print logs every log_stepPrint logs every log_step
 cfg.OUTPUT_DIR = "outputs"
